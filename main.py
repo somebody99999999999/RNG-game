07347a10752f5a98f79b8403e18159f010e13b26
Eduardo roman <student.160097@worcesterschools.net>
2026-06-03 12:54:49 -0400

    update

--- main.py
+++ main.py
@@ -15,37 +15,42 @@ input()
 # Generate the first random pair numbers from between one and any number
 first_num = random.randint(1, 10)
 
-# Generate the second random pair numbers between one and any number
+ # Generate the second random pair numbers between one and any number
 second_num = random.randint(1, 10)
 
 reset_game = True
 
 while True:
-    # Checks if the user won
-    if first_num == 10 and second_num == 10 and reset_game == True:
-        print("Congratulations")
-        input()
-        print("You beat my game")
-        reset_game = True
-    else:
-        # Prints out the first and second set of random numbers
-        print("The numbers you got is ...")
-        input()
-        print(first_num)
-        print(second_num)
-        
-        # So it doesn't reroll if there is a ten aleady on the board 
-        if first_num != 10:
-            first_num = random.randint(1, 10)
+        # Checks if the user won
+        if first_num == 10 and second_num == 10 and reset_game == True:
+            print("Congratulations")
+            input()
+            print("You beat my game")
+            reset_game = True
+        else:
+            # Prints out the first and second set of random numbers
+            print("The numbers you got is ...")
+            print(first_num)
+            print(second_num)
+            
+            # So it doesn't reroll if there is a ten aleady on the board 
+            if first_num != 10:
+                first_num = random.randint(1, 10)
 
-        if second_num != 10:
-            second_num = random.randint(1, 10)
+            if second_num != 10:
+                second_num = random.randint(1, 10)
 
-    # Ask the user for a re-roll
-    reroll = input("You want to get a reroll? (y/n) ").lower()
+        # Ask the user for a re-roll
+        reroll = input("You want to get a reroll? (y/n) ").lower()
 
-    if reroll == "n":
-        print("Bye but thanks for playing my RNG game")
-        
-        # Breaks out of the loop
-        break
+        if reroll == "n":
+            print("Bye but thanks for playing my RNG game")
+            # Breaks out of the loop
+            break
+
+        if reroll == "y":
+            print('ok')
+
+        if reroll == " " or "":
+            print("You Cheat it like that ")
+            
